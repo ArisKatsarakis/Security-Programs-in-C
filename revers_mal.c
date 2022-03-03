4ec2ef4da6c1e774dd49d59810bac1bc913c0473
diaolos <ariskatsarkis@hotmail.com>
2022-03-03 14:35:19 +0200

    Finished reverse Enginnering code

--- revers_mal.c
+++ revers_mal.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
     FILE *read, *write;
     int i , k,len;
-    char buffer[1000];
+    char *buffer;
+    char *decode;
 
     read = fopen("dbb4fdbba0f4bb8b441c1d610260bbc8.VBR","r");
     if(read == NULL){
@@ -14,5 +16,24 @@ int main(){
     fseek(read, 0, SEEK_END);
     len = ftell(read);
     printf("Size of VBR %d \n", len);
+    buffer = malloc(len * sizeof(char));
+    fseek(read,0,SEEK_SET);
+    fread(buffer, len , 1 , read);
+    printf("The encrypted content is: \n %s \n",buffer);
+    for (int i = 0; i < len; i++)
+    {
+        buffer[i] = buffer[i] ^ 0x24;
+        buffer[i] = buffer[i] + 4;
+        if(buffer[i] == '\n'){
+            if (buffer[i+1] == '\n')
+            {
+                decode = malloc(i * sizeof(char));
+                memcpy(decode,buffer,i);
+                break;
+            }
+            
+        }
+    }
+    printf("\nThe  content  after decryption is: \n %s \n",decode);
     
 }
