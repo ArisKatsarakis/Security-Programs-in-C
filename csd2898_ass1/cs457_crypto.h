119f9018febd9f22753a117e154f4dd93f7227fd
diaolos <ariskatsarkis@hotmail.com>
2022-03-29 02:42:09 +0300

    rail_fence Encr completed

--- csd2898_ass1/cs457_crypto.h
+++ csd2898_ass1/cs457_crypto.h
@@ -33,26 +33,63 @@ char* one_time_pad_decr(char  *cipher, char *key){
 }
  
  char* rail_fence_encr(char plain[100] , int rail ){
-     char fence[rail][1000];
-     int  i,j;
+     char fence[rail][100];
+     char* cipher = malloc(strlen(plain) * sizeof(char)) ;
+     int  i,j,k,l;
      //rail making 
-     i =0;
-     while(1){
-        if(i == 0){
-            fence[0][0] = plain[i];
-            j++;
-        }else{
-            fence[j][i] = plain[i];
-        }
-        if(j == rail){
-            j=0;
-        }
-         i++;
+     i=0;
+     j=0;
+     for(k = 0; k<rail;k++){
+        memset(fence[k],'_',strlen(plain)+5);
      }
-     for(int test = 0; test<rail; test++){
-         printf("%s",fence[test]);
+     printf("%d \n",strlen(plain));
+     
+     for ( i = 0; i < strlen(plain);i++)
+     {
+        
+         if(i == 0){
+             fence[0][0] = plain[i];
+             j++;
+             
+         }else{
+            while(j< rail){
+                fence[j][i] = plain[i];
+                j++;
+                i++;
+            }
+            j = j-2;
+            while(j>=0){
+                fence[j][i] = plain[i];
+                j--;
+                i++;
+            }
+             j=1;
+             i--;
+         }
      }
      
+     printf("\n");
+     for(i = 0; i< rail; i++){
+         printf("%s", fence[i]);
+         printf("\n");
+     }
+     i=0;
+     j=0;
+     l=0;
+     //making the cipher
+     for ( i = 0; i < rail; i++)
+     {
+         for ( j = 0; j < strlen(plain) + 5; j++)
+         {
+             if(fence[i][j] != '_' && fence[i][j] != 0){
+                 cipher[l] = fence[i][j];
+                 l++;
+             }
+         }
+         
+     }
+     
+     return cipher;
  }
 
  char* rail_fence_decr(char* cihper, int rail){
--- csd2898_ass1/test.c
+++ csd2898_ass1/test.c
@@ -1,23 +1,11 @@
 #include "cs457_crypto.h"
 
 int main(){
-    char buffer;
-    char plaintext[100], *cipher;
-    int rail =0;
-    int i=0 ;
-     printf("Rail fence cipher \n");
-        while(buffer == '\n'){
-            buffer = getchar();
-            if (buffer == '\n')
-            {
-                break;
-                /* code */
-            }else{
-                plaintext[i] = buffer;
-            }
-            i++;
-            
-        }
-        printf("Give rail number: \n");
-        cipher = rail_fence_encr(plaintext, rail);
+    char buffer[100] = "hello world";
+    char* cipher;
+    printf("Testing\n");
+    printf("Plaintext : %s \n",buffer);
+    cipher = rail_fence_encr(buffer,3);
+    printf("Cipher Text: %s \n", cipher);
+
 }
