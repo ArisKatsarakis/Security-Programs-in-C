4866a7f259c11fa9509b716b5e2aecb6be32918b
diaolos <ariskatsarkis@hotmail.com>
2022-05-02 21:23:28 +0300

    2.2 pcap header parse remain

--- csd2898_assignment2/ids.c
+++ csd2898_assignment2/ids.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-#include "pcap.h"
+#include <pcap.h>
+#include <netinet/in.h>
+#include <netinet/if_ether.h>
+#include <net/ethernet.h>
 
 typedef struct rules{
     char source_ip[100];
@@ -11,12 +14,18 @@ typedef struct rules{
     char destination_ip[100];
     char rules[100];
 } rul;
+
+void my_packet_handler(u_char *args, const struct pcap_pkthdr *header,const u_char *packet);
 int main(){
+    // file opened
+    char error_buffer[PCAP_ERRBUF_SIZE];
+    pcap_t *handle = pcap_open_offline("dhcp.pcapng",error_buffer);
     FILE *rules; 
     char *buffer;
     rul rules_list[100];
     int rul_count = 0;
     int i =1;
+
     rules = fopen("rules.txt", "r");
     if (rules == NULL)
     {
@@ -36,7 +45,24 @@ int main(){
         printf("%s %s %s %s %s \n",rules_list[i].source_ip, rules_list[i].source_port, rules_list[i].destination_port, rules_list[i].destination_ip, rules_list[i].rules );
     }
     //CIDR NOTATION 1.0 = 1.0/24 for src ip
-    
+    //print info of the packet
+    pcap_loop(handle,0,my_packet_handler,NULL);
+    return 0;
 
+    
+}
+void my_packet_handler(u_char *args,
+ const struct pcap_pkthdr *header,const u_char *packet){
+    struct ether_header *eth_header;
+    printf("Header: length: %d \n ",header->caplen);
+    printf("Total-Length: %u\n",header->len);
+    eth_header = (struct ether_header *) packet;
+    for ( int i = 0; i < 6; i++)
+    {
+     printf("%d \n",eth_header->ether_shost[i]);
+
+        /* code */
+    }
+    
     
 }
