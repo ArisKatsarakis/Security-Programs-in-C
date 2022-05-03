518d6d226f157998832d34597cdc0e10b1bead03
diaolos <ariskatsarkis@hotmail.com>
2022-05-04 01:00:57 +0300

    ass2.3 polynomial solved

--- csd2898_assignment2/ass2_3/secret_sharing.c
+++ csd2898_assignment2/ass2_3/secret_sharing.c
@@ -2,41 +2,48 @@
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
-
+// make a and b to be randoms
+// show how to find C in any 3 given points
+void get_num(char *buffer);
+    int f_1,f_2,f_3;
 int main(int argc,char* argv[]){
     int pol_degree = 1;
-    int a = (rand() % (atoi(argv[2])-1) );
-    int secret;
-    int b ;
+    int a = 5;
+    int secret = atoi(argv[2]);
+    int b  = 2;
     int c;
-    int f_1,f_2;
+    int i ;
+    int points[3];
     int x;
     printf("Secret Sharing in C \n");
     if(argc != 3){
         printf("Not enough arguments \n");
     }
-    if (strcmp("split",argv[1]))
+    if (!strcmp("split",argv[1]))
     {
-        printf("Wrong Second Argument \n");
-        /* code */
-    }else{
+        
         printf("Split Option Enabled \n");
         printf("%d \n",a);
-        secret = atoi(argv[2]);
-        //construct the polynomial 
-        //pol_degree =2 
-        x = 1;
-        a = 14;
-        b = 72;
-        f_1 = a*pow(x,pol_degree) + b;
-
-        printf("1 ,%d \n", f_1);
-        x = 2; 
-        a = 14;
-        b = 72;
-        f_2 = a*pow(x,pol_degree) + b;
-        printf("2 ,%d \n", f_2);
-        //solving the polynomial
+        f_1 = a* pow(1,2) + b + secret;
+        f_2 = a* pow(2,2) + 2 * b + secret;
+        f_3 = a* pow(3,2) + 3*b  + secret;
+        printf("(1,%d,2,%d,3,%d)\n",f_1,f_2,f_3);
+    }else if(!strcmp("join",argv[1])){
+        char *buffer[100];
+        printf("Join Option Enabled \n");
+        printf("%s \n", argv[2]);
+        //parse argv[2]
+        get_num(argv[2]);
+        //parse the command of ./secret_sharing
+        // c = f_3 + 3* f_1 - 3  * f_2;
+        // printf("Secret is %d \n",c);
+    }else{
+        printf("Wrong Second Argument \n");
+        return 1;
     }
-    
+    return 0;
+}
+void get_num(char* buffer){
+    printf("getting nums \n");
+
 }
