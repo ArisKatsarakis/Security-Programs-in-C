fc87de529ef3468858863d96896d3d8eec6d30fe
aris.katsarakis <aris.katsarakis@hrepathsolutions.com>
2023-03-22 21:30:59 +0200

    changes for encrypting in the header file

--- assignment12023/cs457_crypto.c
+++ assignment12023/cs457_crypto.c
@@ -1,16 +1,48 @@
+#define cs457_crypto_c
 #include "cs457_crypto.h"
+void createRandomData() {
+            int i;
+            unsigned int buffer[8];
+            int fd = open("/dev/urandom", O_RDONLY);
+            read(fd, buffer, 8);
+            //buffer now contains the random data
+            close(fd);
+            for(i = 0; i < 1; ++i)
+                    printf("%02X", buffer[i]);
+            printf("\n");
+            printf("%d \n",buffer[0]);
+}
 int main ( ) {
     printHelloWorld();
-    char  *encryption = one_time_pad_encr("Testing", sizeof("Testing"));
+    const char* encryption = one_time_pad_encr("Testing", sizeof("Testing"), &encryptionKey);
+    const char* decryption = one_time_pad_denr(&encryption, sizeof("Testing"),&encryptionKey);
     printf("%s \n",encryption );
+    printf("%s \n",decryption );
     return 0;
     
 }
 
 void printHelloWorld( ) {
        printf("Hello World \n ");
-
 }
-const char* one_time_pad_encr(char *plaintext, int plainSize ) {
-    return *plaintext;
+
+
+
+const char* one_time_pad_encr(char *plaintext, int plainSize, int *secretKey ) {
+    int i =0;
+    char* ciphertext;
+    ciphertext = malloc(plainSize * sizeof( char));
+    *secretKey = rand();
+    for(i =0; i < plainSize; i++)
+       ciphertext[i] = (char) (plaintext[i] ^ 'x');
+    return ciphertext;
+}
+
+const char*  one_time_pad_denr(char *ciphertext, int plainSize, int *secretKey) {
+
+    int i =0;
+    char* plaintext = malloc(plainSize * sizeof(char));
+    for(i =0; i < plainSize; i++)
+           plaintext[i] = (char) (ciphertext[i] ^ 'x');
+    return plaintext;
 }
--- assignment12023/cs457_crypto.h
+++ assignment12023/cs457_crypto.h
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+#ifdef cs457_crypto_c
+#define EXTERN
+#else
+#define EXTERN extern
+#endif
+
+EXTERN int encryptionKey;
 /**
  * printing hello world
 */
 
 void printHelloWorld(void);
+/**
+Obe time pad Encryption uses a plaintext and its size as a number in order to create the cipher text by parsing the whole plain text and XOR-ing its letter of it with the plainSize
+*/
 
+const char*  one_time_pad_encr(char *plaintext, int plainSize, int *secretKey);
 
-const char*  one_time_pad_encr(char *plaintext, int plainSize );
+const char*  one_time_pad_denr(char *ciphertext, int plainSize, int *secretKey);
